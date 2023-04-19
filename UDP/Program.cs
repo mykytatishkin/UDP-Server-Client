a9c3216055ac50f8ca7ab6a384b26167da26888a
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-04-19 18:56:56 +0300

    init

---
+++ UDP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            socket.Bind(new IPEndPoint(IPAddress.Loopback, 11000));
+            try
+            {
+                while(true)
+                {
+                    Console.WriteLine("Waiting for client...");
+                    EndPoint remoteEp = new IPEndPoint(IPAddress.Loopback, 0);
+                    var buffer = new byte[4096];
+                    int size = socket.ReceiveFrom(buffer, ref remoteEp);
+                    Console.WriteLine($"Recived: {Encoding.UTF8.GetString(buffer)}");
+                    socket.SendTo(Encoding.UTF8.GetBytes("Hi from Server. Message was recived"), remoteEp);
+                }
+            } 
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ UDP_Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace UDP_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            try
+            {
+                Console.WriteLine("Enter message");
+                var msg = Console.ReadLine();
+                var serverEp = new IPEndPoint(IPAddress.Loopback, 11000);
+                socket.SendTo(Encoding.UTF8.GetBytes(msg), serverEp);
+
+                var buffer = new byte[1024];
+                EndPoint remoteEp = new IPEndPoint(IPAddress.Loopback, 0);
+                int size = socket.ReceiveFrom(buffer, ref remoteEp);
+                Console.WriteLine($"From Server: {Encoding.UTF8.GetString(buffer)}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
